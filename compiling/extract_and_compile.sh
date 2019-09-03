62b0ede84822fece2ef37b948032e466ab35a9a0
Ariel Cordes <corde171@morris.umn.edu>
2019-09-03 10:33:31 -0500

    Compilation tests all pass and shellcheck passed

--- compiling/extract_and_compile.sh
+++ compiling/extract_and_compile.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-test(){
-echo "5"
-}
-test
-pwd
+NUM=$1
+DIR_NAME=$2
+
+tar -zxf "NthPrime.tgz" --directory $DIR_NAME
+cd $DIR_NAME
+cd "NthPrime"
+gcc -o NthPrime main.c nth_prime.c nth_prime.h
+./NthPrime $NUM
 
 
