d767d225db87095ffee3ed29dbd233df807c2f5a
corde171 <corde171@morris.umn.edu>
2019-09-04 13:32:29 -0500

    Run shellcheck and clean the cleaner

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -1,12 +1,11 @@
-
 #!/bin/bash
 chmod +x  big_clean.sh 
 NAME=$1
 SCRATCH=$2
-echo $SCRATCH
+
 HERE=$(pwd)
 
 tar zxf "$NAME" --directory "$SCRATCH"
-grep -lr "DELETE ME!" $SCRATCH | xargs rm -fr
-cd $SCRATCH
-tar -zcf "$HERE"/"cleaned_$NAME" *
+grep -lr "DELETE ME!" "$SCRATCH" | xargs rm -fr
+cd "$SCRATCH" || exit
+tar -zcf "$HERE/cleaned_$NAME" *
