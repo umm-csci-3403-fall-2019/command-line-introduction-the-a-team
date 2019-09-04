b5dc2b2c214521f111af42dc9f9f70d3cec78827
Joshua Hamann <haman127@pegasus.morris.umn.edu>
2019-09-04 12:21:01 -0500

    adds grep, and chmod

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -1,11 +1,12 @@
+
 #!/bin/bash
+chmod +x  big_clean.sh 
 NAME=$1
 SCRATCH=$2
+echo $SCRATCH
 HERE=$(pwd)
 
-tar -zxf "$NAME" --directory "$SCRATCH"
-grep -lr "DELETE ME!" $SCRATCH | xargs rm	
-
-
-
-
+tar zxf "$NAME" --directory "$SCRATCH"
+grep -lr "DELETE ME!" $SCRATCH | xargs rm -fr
+cd $SCRATCH
+tar -zcf "$HERE"/"cleaned_$NAME" *
